e85f7660f1b96efbb5f40ec842ce178eab03ceb0
Richard Littauer <richard.littauer@gmail.com>
2016-01-14 13:49:18 -0500

    Fixed tests, linted, added babel

--- app/index.js
+++ app/index.js
@@ -1,78 +1,78 @@
-'use strict';
-const normalizeUrl = require('normalize-url');
-const humanizeUrl = require('humanize-url');
-const yeoman = require('yeoman-generator');
-const _s = require('underscore.string');
+'use strict'
+const normalizeUrl = require('normalize-url')
+const humanizeUrl = require('humanize-url')
+const yeoman = require('yeoman-generator')
+const _s = require('underscore.string')
 
 module.exports = yeoman.Base.extend({
-	init() {
-		const cb = this.async();
-		const self = this;
+  init () {
+    const cb = this.async()
+    const self = this
 
-		this.prompt([{
-			name: 'moduleName',
-			message: 'What do you want to name your module?',
-			default: this.appname.replace(/\s/g, '-'),
-			filter: x => _s.slugify(x)
-		}, {
-			name: 'githubUsername',
-			message: 'What is your GitHub username?',
-			store: true,
-			validate: x => x.length > 0 ? true : 'You have to provide a username'
-		}, {
-			name: 'website',
-			message: 'What is the URL of your website?',
-			store: true,
-			validate: x => x.length > 0 ? true : 'You have to provide a website URL',
-			filter: x => normalizeUrl(x)
-		}, {
-			name: 'description',
-			message: 'What is the description of this module?',
-			store: true,
-			validate: x => x.length > 0 ? true : 'You have to provide a module description'
-		}, {
-			name: 'cli',
-			message: 'Do you need a CLI?',
-			type: 'confirm',
-			default: false
-		}], props => {
-			const tpl = {
-				moduleName: props.moduleName,
-				camelModuleName: _s.camelize(props.moduleName),
-				githubUsername: props.githubUsername,
-				name: self.user.git.name(),
-				email: self.user.git.email(),
-				website: props.website,
-				humanizedWebsite: humanizeUrl(props.website),
-				description: props.description,
-				cli: props.cli
-			};
+    this.prompt([{
+      name: 'moduleName',
+      message: 'What do you want to name your module?',
+      default: this.appname.replace(/\s/g, '-'),
+      filter: x => _s.slugify(x)
+    }, {
+      name: 'githubUsername',
+      message: 'What is your GitHub username?',
+      store: true,
+      validate: x => x.length > 0 ? true : 'You have to provide a username'
+    }, {
+      name: 'website',
+      message: 'What is the URL of your website?',
+      store: true,
+      validate: x => x.length > 0 ? true : 'You have to provide a website URL',
+      filter: x => normalizeUrl(x)
+    }, {
+      name: 'description',
+      message: 'What is the description of this module?',
+      store: true,
+      validate: x => x.length > 0 ? true : 'You have to provide a module description'
+    }, {
+      name: 'cli',
+      message: 'Do you need a CLI?',
+      type: 'confirm',
+      default: false
+    }], props => {
+      const tpl = {
+        moduleName: props.moduleName,
+        camelModuleName: _s.camelize(props.moduleName),
+        githubUsername: props.githubUsername,
+        name: self.user.git.name(),
+        email: self.user.git.email(),
+        website: props.website,
+        humanizedWebsite: humanizeUrl(props.website),
+        description: props.description,
+        cli: props.cli
+      }
 
-			const mv = (from, to) => {
-				self.fs.move(self.destinationPath(from), self.destinationPath(to));
-			};
+      const mv = (from, to) => {
+        self.fs.move(self.destinationPath(from), self.destinationPath(to))
+      }
 
-			self.fs.copyTpl([
-				`${self.templatePath()}/**`,
-				'!**/cli.js'
-			], self.destinationPath(), tpl);
+      self.fs.copyTpl([
+        `${self.templatePath()}/**`,
+        '!**/cli.js'
+      ], self.destinationPath(), tpl)
 
-			if (props.cli) {
-				self.fs.copyTpl(self.templatePath('cli.js'), self.destinationPath('cli.js'), tpl);
-			}
+      if (props.cli) {
+        self.fs.copyTpl(self.templatePath('cli.js'), self.destinationPath('cli.js'), tpl)
+      }
 
-			mv('gitignore', '.gitignore');
-			mv('gitattributes', '.gitattributes');
-			mv('travis.yml', '.travis.yml');
-			mv('_package.json', 'package.json');
+      mv('gitignore', '.gitignore')
+      mv('gitattributes', '.gitattributes')
+      mv('travis.yml', '.travis.yml')
+      mv('_package.json', 'package.json')
 
-			cb();
-		});
-	},
-	git() {
-		this.spawnCommandSync('git', ['init']);
-	},
-	install() {
-		this.installDependencies({bower: false});
-	}
-});
+      cb()
+    })
+  },
+  git () {
+    this.spawnCommandSync('git', ['init'])
+  },
+  install () {
+    this.installDependencies({bower: false})
+  }
+})
--- test.js
+++ test.js
@@ -1,54 +1,55 @@
-import path from 'path';
-import test from 'ava';
-import helpers from 'yeoman-test';
-import assert from 'yeoman-assert';
-import pify from 'pify';
+import path from 'path'
+import test from 'ava'
+import helpers from 'yeoman-test'
+import assert from 'yeoman-assert'
+import pify from 'pify'
 
-let generator;
+let generator
 
 test.beforeEach(async () => {
-	await pify(helpers.testDirectory)(path.join(__dirname, 'temp'));
-	generator = helpers.createGenerator('nm:app', ['../app'], null, {skipInstall: true});
-});
+  await pify(helpers.testDirectory)(path.join(__dirname, 'temp'))
+  generator = helpers.createGenerator('nms:app', ['../app'], null, {skipInstall: true})
+})
 
 test.serial('generates expected files', async () => {
-	helpers.mockPrompt(generator, {
-		moduleName: 'test',
-		githubUsername: 'test',
-		website: 'test.com',
-		cli: false
-	});
+  helpers.mockPrompt(generator, {
+    moduleName: 'test',
+    githubUsername: 'test',
+    website: 'test.com',
+    description: 'test',
+    cli: false
+  })
 
-	await pify(generator.run.bind(generator))();
+  await pify(generator.run.bind(generator))()
 
-	assert.file([
-		'.editorconfig',
-		'.git',
-		'.gitattributes',
-		'.gitignore',
-		'.travis.yml',
-		'index.js',
-		'license',
-		'package.json',
-		'readme.md',
-		'test.js'
-	]);
+  assert.file([
+    '.git',
+    '.gitattributes',
+    '.gitignore',
+    '.travis.yml',
+    'index.js',
+    'license',
+    'package.json',
+    'readme.md',
+    'test.js'
+  ])
 
-	assert.noFile('cli.js');
-});
+  assert.noFile('cli.js')
+})
 
 test.serial('CLI option', async () => {
-	helpers.mockPrompt(generator, {
-		moduleName: 'test',
-		githubUsername: 'test',
-		website: 'test.com',
-		cli: true
-	});
+  helpers.mockPrompt(generator, {
+    moduleName: 'test',
+    githubUsername: 'test',
+    website: 'test.com',
+    description: 'test',
+    cli: true
+  })
 
-	await pify(generator.run.bind(generator))();
+  await pify(generator.run.bind(generator))()
 
-	assert.file('cli.js');
-	assert.fileContent('package.json', /"bin":/);
-	assert.fileContent('package.json', /"bin": "cli.js"/);
-	assert.fileContent('package.json', /"meow"/);
-});
+  assert.file('cli.js')
+  assert.fileContent('package.json', /"bin":/)
+  assert.fileContent('package.json', /"bin": "cli.js"/)
+  assert.fileContent('package.json', /"meow"/)
+})
